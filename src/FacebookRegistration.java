28bb45c6fc8e2bfb50fa6bd9da9f3cfc3c533d8c
deniz <127349458+kyzuwang@users.noreply.github.com>
2023-07-11 14:36:52 -0700

    a

--- src/FacebookRegistration.java
+++ src/FacebookRegistration.java
@@ -2,5 +2,6 @@ public class FacebookRegistration {
     public static void main(String[] args) {
         System.out.println("example");
         System.out.println("example2");
+        System.out.println("example3");
     }
 }
