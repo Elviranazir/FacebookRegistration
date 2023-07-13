7fead751b81f39ed43b0b87dc612ba88886029e9
Elvira <elvira.nzr@gmail.com>
2023-07-13 16:33:39 -0400

    my Codes

--- src/FacebookRegistration.java
+++
@@ -1,7 +0,0 @@
-public class FacebookRegistration {
-    public static void main(String[] args) {
-        System.out.println("example");
-        System.out.println("example2");
-        System.out.println("example3");
-    }
-}
