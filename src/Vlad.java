9353e02981a8beaf96c576cbb34c5650047839d6
vladmak <vladmak78@gmail.com>
2023-07-13 10:13:44 -0500

    new facebook update

--- src/Vlad.java
+++ src/Vlad.java
@@ -1,5 +1,17 @@
+/*
+
+
+
 public class Vlad {
     public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
 
+        WebElement PhoneNumber = driver.findElement(By.cssSelector("input[id^='u_'][name='reg_email__']"));
+        MyMethods.myWait(1);
+        PhoneNumber.sendKeys("6123221231");
     }
 }
+
+
+*/
