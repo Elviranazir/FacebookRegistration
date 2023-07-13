d6c85ba2f8cbf078f2cea3803e93a7bfa0787a2f
Elvira Nazir <131047123+Elviranazir@users.noreply.github.com>
2023-07-13 16:28:27 -0400

    Delete MyMethods.java

--- src/MyMethods.java
+++
@@ -1,9 +0,0 @@
-public class MyMethods {
-    public static void myWait(int time){
-        try {
-            Thread.sleep(time*1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-}
