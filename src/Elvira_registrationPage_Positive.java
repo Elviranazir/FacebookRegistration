1e0154b7453889a57646c4b816e038bbdc598f52
Elvira <elvira.nzr@gmail.com>
2023-07-15 15:13:04 -0400

    facebook registration

--- src/Elvira_registrationPage_Positive.java
+++ src/Elvira_registrationPage_Positive.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 
-public class Elvira_FirstName_Positive extends BaseDriver {
+public class Elvira_registrationPage_Positive extends BaseDriver {
     @Test
     public void faceBookCreateAccount() {
         driver.get("https://www.facebook.com/");
